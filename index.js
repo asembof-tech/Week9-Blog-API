feafafa4b5b65ff1fc39cb4463702411791cfc73
Festus Asembo <asembof@gmail.com>
2026-09-03 18:41:21 +0100

    fix port

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`API alive on port ${PORT}`);
-  });
+  const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
