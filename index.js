0e235747a18239b5a4092b9ea04a0aa8b8c58066
Festus Asembo <asembof@gmail.com>
2026-09-11 00:26:19 +0100

    Fixed auth routes in index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+const authRoutes = require('./routes/authRoutes');
 
 app.use("/articles", articleRoutes);
 
