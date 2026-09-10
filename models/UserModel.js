e0c388fc650db7c97145e291c33860fbd53a34a8
Festus Asembo <asembof@gmail.com>
2026-09-11 00:57:29 +0100

    Fixed Mongoose next() error in UserModel

--- models/UserModel.js
+++ models/UserModel.js
@@ -20,13 +20,12 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Pre-save hook: Hash password before saving to the database
-userSchema.pre('save', async function(next) {
-    // Only hash the password if it has been modified (or is new)
-    if (!this.isModified('password')) return next();
+// NOTE: No "next" used here. We just use "return" because it's an async function.
+userSchema.pre('save', async function() {
+    if (!this.isModified('password')) return;
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 // Method to compare entered password with hashed password in the database
